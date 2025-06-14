529273b892285f6507ad0561ba5434dc3dd3a6ee
nevillemthw <nevillemathew2014@gmail.com>
2025-06-14 17:25:19 -0500

    Changed title

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { SpeedInsights } from "@vercel/speed-insights/next"
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'Stopover Frontend',
+  title: 'Stopover',
   description: 'Frontend for stopover flight website'
 }
 
